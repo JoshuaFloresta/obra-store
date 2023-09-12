10500cbcd37bc2b44b33f85f52914c627cb8d232
JoshuaFloresta <florestajoshua.dev@gmail.com>
2023-09-12 17:14:07 +0800

    redeploy

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,18 +1,26 @@
 "use client";
 
-import Link from "next/link"
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 import { Category } from "@/types";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown";
+import { ChevronDown } from "lucide-react";
 
 interface MainNavProps {
   data: Category[];
 }
 
-const MainNav: React.FC<MainNavProps> = ({
-  data
-}) => {
+const MainNav: React.FC<MainNavProps> = ({ data }) => {
   const pathname = usePathname();
 
   const routes = data.map((route) => ({
@@ -22,23 +30,54 @@ const MainNav: React.FC<MainNavProps> = ({
   }));
 
   return (
-    <nav
-      className="mx-6 flex items-center space-x-4 lg:space-x-6"
-    >
-      {routes.map((route) => (
-        <Link
-          key={route.href}
-          href={route.href}
-          className={cn(
-            'text-sm font-medium transition-colors hover:text-black',
-            route.active ? 'text-black' : 'text-neutral-500'
-          )}
-        >
-          {route.label}
+    <nav className=" mx-auto flex items-center space-x-4 lg:space-x-6">
+      <Link
+        href="/store"
+        className="font-semibold transition-colors hover:text-black ms-6"
+      >
+        <p>Featured</p>
       </Link>
-      ))}
+
+      <DropdownMenu>
+        <DropdownMenuTrigger className="flex font-semibold  transition-colors hover:text-black ">
+          Categories
+          <ChevronDown className="text-neutral-800" strokeWidth={1} />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="bg-white">
+          <DropdownMenuSeparator />
+          {routes.map((route) => (
+            <DropdownMenuItem className="bg-white">
+              <Link
+                key={route.href}
+                href={route.href}
+                className={cn(
+                  "text-sm font-medium transition-colors hover:text-black",
+                  route.active ? "text-black" : "text-neutral-500"
+                )}
+              >
+                {route.label}
+              </Link>
+            </DropdownMenuItem>
+          ))}
+          <DropdownMenuItem className="font-semibold  transition-colors hover:text-black ms-6"></DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <Link
+        href="/artist"
+        className="font-semibold  transition-colors hover:text-black ms-6"
+      >
+        <p>Popular Artist</p>
+      </Link>
+
+      <Link
+        href="/store"
+        className="font-semibold  transition-colors hover:text-black ms-6"
+      >
+        <p>About Us</p>
+      </Link>
     </nav>
-  )
+  );
 };
 
 export default MainNav;
